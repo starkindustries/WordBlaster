0808b4d0f239c6d6a308b3b92533d15e86f5b84a
Zion Perez <zionperez@gmail.com>
2019-05-20 02:36:29 +0900

    First commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject counterText;
+    public int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Increment()
+    {
+        Debug.Log("Increment!");
+        counter++;
+        counterText.GetComponentInChildren<Text>().text = "Count: " + counter.ToString();
+    }
+
+    public void DidPressKey(string key)
+    {
+        Debug.Log(key);
+        counterText.GetComponentInChildren<Text>().text = key;
+    }
+}
---
+++ Assets/Scripts/KeyboardButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardButton : MonoBehaviour
+{
+    public Text uiText; 
+    public string key;
+
+    public void Start()
+    {
+        uiText.text = key;
+    }
+
+    public void DidPressKey()
+    {
+        FindObjectOfType<GameManager>().DidPressKey(key);
+    }
+}
