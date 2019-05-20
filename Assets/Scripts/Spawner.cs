1721dc2397991ef4ec5b2c6dc364f7ec2976ffa0
Zion Perez <zionperez@gmail.com>
2019-05-20 20:56:55 +0900

    Implement spawner to spawn falling blobs.

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float minX;
+    public float maxX;
+    public float minY;
+
+    public GameObject spawn;
+    public float spawnRate;
+
+    private float nextSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            float randX = Random.Range(minX, maxX);
+            Vector2 location = new Vector2(randX, minY);
+            Instantiate(spawn, location, Quaternion.identity);
+        }
+    }
+}
