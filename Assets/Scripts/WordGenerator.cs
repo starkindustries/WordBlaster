643941f91e7dc4588beb39760c398eb7736b185d
Zion Perez <zionperez@gmail.com>
2019-05-21 17:19:36 +0900

    Replace words with letters. Clean-up WordSpawner class; remove old unused code.

--- Assets/Scripts/WordGenerator.cs
+++ Assets/Scripts/WordGenerator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class WordGenerator: MonoBehaviour
 {
-    private static string[] wordList = { "test", "magic", "awesome", "game", "programmer" };
+    private static string[] wordList = { "q", "w", "e", "r", "t", "y", "u", "i", "o", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"};
 
     public static string GetRandomWord()
     {
--- Assets/Scripts/WordSpawner.cs
+++ Assets/Scripts/WordSpawner.cs
@@ -9,35 +9,13 @@ public class WordSpawner : MonoBehaviour
     public float minY;
 
     public GameObject wordPrefab;
-    public float spawnRate;
     public Transform wordCanvas;
-
-    private float nextSpawn;
 
     public WordDisplay SpawnWord()
     {
-        Vector3 randomPosition = new Vector3(Random.Range(-2.5f, 2.5f), 7f);
-
+        Debug.Log("Spawn Word");
+        Vector3 randomPosition = new Vector3(Random.Range(minX, maxX), minY);
         GameObject wordObj = Instantiate(wordPrefab, randomPosition, Quaternion.identity, wordCanvas);
         return wordObj.GetComponent<WordDisplay>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    /*
-    // Update is called once per frame
-    void Update()
-    {
-        if(Time.time > nextSpawn)
-        {
-            nextSpawn = Time.time + spawnRate;
-            float randX = Random.Range(minX, maxX);
-            Vector2 location = new Vector2(randX, minY);
-            Instantiate(wordPrefab, location, Quaternion.identity);
-        }
-    }
-    */
+    }    
 }
