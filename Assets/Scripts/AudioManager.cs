465e2ceed3f4d30d5bee19cec605ebd070dad80c
Zion Perez <zionperez@gmail.com>
2019-05-22 20:37:35 +0900

    Implement AudioManager and theme sound.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    // Singleton pattern
+    // https://gamedev.stackexchange.com/a/116010/123894
+    private static AudioManager _instance;
+    public static AudioManager Instance { get { return _instance; } }
+
+    public Sound[] sounds;
+
+    private void Awake()
+    {
+        // Singleton Enforcement Code
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        // AudioMAnager Code
+        foreach (Sound sound in sounds)
+        {
+            // Add a new AudioSource component for each playable sound
+            sound.SetSource(gameObject.AddComponent<AudioSource>());
+        }
+    }
+
+    private void Start()
+    {
+        Play("Theme");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound " + name + " not found!");
+            return;
+        }
+        s.Play();
+    }
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(.1f, 3f)]
+    public float pitch;
+    public bool loop;
+
+    private AudioSource source;
+
+    public void SetSource(AudioSource newSource)
+    {
+        // Copy the sound's properties to the new audio source
+        source = newSource;
+        source.clip = clip;
+        source.volume = volume;
+        source.pitch = pitch;
+        source.loop = loop;
+    }
+
+    public void Play()
+    {
+        Debug.Log("Playing: " + name);
+        source.Play();
+    }
+}
