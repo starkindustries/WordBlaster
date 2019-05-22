4a2e145dd5f10e16730c52ad4a3fc50f9561e24c
Zion Perez <zionperez@gmail.com>
2019-05-22 15:22:05 +0900

    Add player placeholder and bullet prefab with placeholder scripts. Implement bullet to fire upwards on game start.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{    
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    // public GameObject bulletParticles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+        Destroy(this.gameObject, 3.0f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("BULLET HIT: " + collision.gameObject.tag);
+        // Instantiate(bulletParticles, transform.position, transform.rotation);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    private void Start()
+    {
+        Shoot();
+    }    
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
