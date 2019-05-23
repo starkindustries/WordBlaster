a691a1552657fd13854303681a6b7d7a68e177d2
Zion Perez <zionperez@gmail.com>
2019-05-23 22:29:17 +0900

    Implement scrolling background.

---
+++ Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour
+{
+    private float backgroundHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundHeight = GetComponent<BoxCollider2D>().size.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < backgroundHeight * -1.5)
+        {
+            RepositionBackground();
+        }
+        Debug.Log("bg height: " + backgroundHeight + ", transform.position.y: " + transform.position.y);
+    }
+
+    private void RepositionBackground()
+    {
+        Vector2 groundOffset = new Vector2(0, backgroundHeight * 2f);
+        transform.position = (Vector2)transform.position + groundOffset;
+    }
+
+}
---
+++ Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+    public float scrollSpeed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(0, -scrollSpeed);
+    }
+}
