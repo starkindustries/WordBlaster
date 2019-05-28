5ca18a126bdddd91df7df759b2b771513091f166
Zion Perez <zionperez@gmail.com>
2019-05-28 15:30:30 +0900

    Add flashcard, flashcard display (placeholder), and testManager to test cards.

---
+++ Assets/Scripts/Flashcard.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashcard
+{
+    private string front;
+    private string back;
+
+    public Flashcard(string front, string back)
+    {
+        this.front = front;
+        this.back = back;
+    }
+
+    // Check Match Functions
+    public bool DoesMatchFront(string front)
+    {
+        return this.front == front;
+    }
+
+    public bool DoesMatchBack(string back)
+    {
+        return this.back == back;
+    }
+
+    // Getter Functions
+    public string GetFront()
+    {
+        return front;
+    }
+
+    public string GetBack()
+    {
+        return back;
+    }
+}
---
+++ Assets/Scripts/TestManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TestFlashcard();
+    }    
+
+    public void TestFlashcard()
+    {
+        Debug.Log("Testing Flashcards");
+        Flashcard card1 = new Flashcard("testFront", "testBack");
+        Debug.Assert(card1.GetFront() == "testFront", "Flashcard error: Front of cards do NOT match!");        
+        Debug.Assert(card1.GetBack() == "testBack", "Flashcard error: Back of cards do NOT match!");
+        Debug.Assert(card1.DoesMatchFront("testFront"), "Flashcard error: Front of cards do NOT match!");
+        Debug.Assert(card1.DoesMatchBack("testBack"), "Flashcard error: Back of cards do NOT match!");
+
+        Flashcard card2 = new Flashcard("산", "mountain");
+        Debug.Assert(card2.GetFront() == "산", "Flashcard error: Front of cards do NOT match!");        
+        Debug.Assert(card2.GetBack() == "mountain", "Flashcard error: Back of cards do NOT match!");
+        Debug.Assert(card2.DoesMatchFront("산"), "Flashcard error: Front of cards do NOT match!");
+        Debug.Assert(card2.DoesMatchBack("mountain"), "Flashcard error: Back of cards do NOT match!");
+    }
+}
