244769c09aa526962c61d060536da9c1966e15ab
Zion Perez <zionperez@gmail.com>
2019-06-08 16:11:28 +0900

    Implement LevelChanger with FadeIn and FadeOut animations.

---
+++ Assets/LevelChanger.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+
+    private int sceneToLoad;
+
+    // Singleton pattern
+    // https://gamedev.stackexchange.com/a/116010/123894
+    private static LevelChanger _instance;
+    public static LevelChanger Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        // Singleton Enforcement Code
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            _instance = this;
+        }
+        DontDestroyOnLoad(gameObject);        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            FadetoScene(1);
+        }
+    }
+
+    public void FadetoScene(int sceneIndex)
+    {
+        sceneToLoad = sceneIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+        animator.SetTrigger("FadeIn");
+    }
+}
